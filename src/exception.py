f7f024f0d42faf1624cbe934ff70bc0e6534e366
sakshi <grawalsakshi01@gmail.com>
2026-03-15 23:28:35 +0530

    add the changes

--- src/exception.py
+++ src/exception.py
@@ -19,7 +19,7 @@ class CustomException(Exception):
         
 if __name__=="__main__":
     try:
-        a=1/10
+        a=1/0
     except Exception as e:
         logging.info("Divide by zero ")
         raise CustomException(e,sys)
