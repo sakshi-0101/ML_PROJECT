2a713cbd1cd7921bc01f524ab902d6db99369a29
sakshi <grawalsakshi01@gmail.com>
2026-03-17 23:04:17 +0530

    hypertuning is done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-from src.components.model_trainer import ModelTrainingConfig
+from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
